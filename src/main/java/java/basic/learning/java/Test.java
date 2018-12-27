876ce8c7b983fe47d0379ea0682dfa6e7e5bde48
apple <603949699@qq.com>
2018-12-27 23:16:36 +0800

    初始化

---
+++ src/main/java/java/basic/learning/java/Test.java
@@ -0,0 +1,10 @@
+package java.basic.learning.java;
+
+/**
+ * @author ：apple
+ * @description ：
+ * @copyright ：	Copyright 2018 yowits Corporation. All rights reserved.
+ * @create ：2018/12/27 下午11:16
+ */
+public class Test {
+}
