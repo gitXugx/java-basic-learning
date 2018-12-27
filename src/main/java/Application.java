bbdb841f3e0e22ed19b222dbb8706c52ae760705
gitXugx <603949699@qq.com>
2018-12-27 15:28:02 +0800

    初始化项目

---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+/**
+ * @author ：ex-xugaoxiang001
+ * @description ：
+ * @copyright ：	Copyright 2018 yowits Corporation. All rights reserved.
+ * @create ：2018/12/27 15:26
+ */
+public class Application {
+
+
+}
