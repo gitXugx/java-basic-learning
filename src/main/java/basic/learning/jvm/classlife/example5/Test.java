bc38d041adc84047ecee77443daafc1cc9284dad
gitXugx <603949699@qq.com>
2019-01-02 17:02:36 +0800

    修改classlifecycle文件

---
+++ src/main/java/basic/learning/jvm/classlife/example5/Test.java
@@ -0,0 +1,12 @@
+package basic.learning.jvm.classlife.example5;
+
+/**
+ * @author ：apple
+ * @description ：
+ * @copyright ：	Copyright 2019 yowits Corporation. All rights reserved.
+ * @create ：2019/1/1 下午9:45
+ */
+public class Test {
+    public  static void main(String[] args){
+    }
+}
